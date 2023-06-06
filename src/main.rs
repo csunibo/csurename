5d8422df3dbda72911e4db414e63ddbb8f5c0d61
jgabaut <109908086+jgabaut@users.noreply.github.com>
2023-06-06 12:04:32 +0200

    feat(lib.rs): new_filter() force textmode
    Adds a new constructor for Config.
    I tried other things at random (monkey) and could not figure out how I can have this thing filter,
    without passing an actual argument on command line.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,13 @@ use csurename::Config;
 use std::process;
 
 fn main() {
-    let config = Config::new().unwrap_or_else(|err| {
+    println!("csurename:  Running in filter mode, empty line \'$^\' or CTRL-C to quit.\n");
+    //But how to correctly impl / wrap Config?
+    //
+    //New pub constructors as big as this one cannot be the answer.
+    //Help me
+
+    let config = Config::new_filter().unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
